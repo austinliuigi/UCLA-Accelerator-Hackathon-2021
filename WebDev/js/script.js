cfff3adfd7aaab0cf617e2b34aaa31a15ca09c8d
Brian Burrous <brianon007@gmail.com>
2021-08-22 12:13:42 -0700

    Updated website code

--- WebDev/js/script.js
+++ WebDev/js/script.js
@@ -5,5 +5,7 @@ $("button").click(function () {
     $.get("https://official-joke-api.appspot.com/random_joke", function (data, status) {
         jokeSetup.innerHTML = data.setup
         jokePunchline.innerHTML = "> " + data.punchline
+        let jokeButton = document.getElementById("jokeBtn")
+        jokeButton.scrollIntoView();
     });
 });
